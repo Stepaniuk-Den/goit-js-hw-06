1941211bf14c1e5fa17358304e9f32876d790125
Stepaniuk-Den <128968908+Stepaniuk-Den@users.noreply.github.com>
2023-06-06 16:32:51 +0300

    update-task07

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,8 @@
 const fontInput = document.querySelector("#font-size-control");
 const textSize = document.querySelector("#text");
 
+textSize.style.fontSize = `${fontInput.value}px`;
+
 fontInput.addEventListener("input", handleTextSizeInput);
 
 function handleTextSizeInput() {
